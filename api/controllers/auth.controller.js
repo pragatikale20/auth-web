6b76dd747145b92cb9f282a5867c8477357912a6
Pragati Kale <pragatikale20803@gmail.com>
2025-01-12 21:57:19 +0530

    add a middleware and a function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from '../modules/user model.js';
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async (req , res ) => {
+export const signup = async (req , res , next) => {
 
  const { username, email, password } = req.body;
  const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -12,8 +13,7 @@ try {
     
 } catch(error)
 {
-        res.status(500).json(error.message);
-    
+       next(error);
 }
 
 };
--- api/index.js
+++ api/index.js
@@ -20,6 +20,19 @@ app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/auth" , authRoutes);
 
+app.use(( err , req , res , next ) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.statusCode(statusCode).json({ 
+    success: false ,
+    message ,
+    statusCode,
+   });
+
+});
+
 app.listen(3000, () => {
+
   console.log('Server is running on http://localhost:3000');
+
 });
